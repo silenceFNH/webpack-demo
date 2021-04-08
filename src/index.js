431ac2dd79ceba2e24ac0a4041abc30327a15790
silence <867943221@qq.com>
2021-04-08 18:13:06 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import x from "./x.js";
-console.log(x);
-console.log("hi");
+import png from "./assets/1.png";
+console.log(png);
+
+const div = document.getElementById("app");
+console.log("div");
+console.log(div);
+div.innerHTML = `
+<img src="${png}">
+`;
+
+const button = document.createElement("button");
+button.innerText = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {}
+  );
+};
+
+div.appendChild(button);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,4 +12,45 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.styl$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "stylus-loader",
+          },
+        ],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "less-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
